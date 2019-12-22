4d7e33b011dfeb81d080f00c69dd8a75086eb3f1
Gokul Nath VM <gnathvm@gmail.com>
2019-12-22 22:01:25 +0530

    Fix syntax

--- android/variable.go
+++ android/variable.go
@@ -126,10 +126,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
-
-          // include Sosp variables
-          Sosp android.Product_variables
-          
+		
+		// include Sosp variables
+		Sosp android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -293,8 +292,8 @@ type productVariables struct {
 	ProductHiddenAPIStubsTest   []string `json:",omitempty"`
 
 	TargetFSConfigGen []string `json:",omitempty"`
-          // include Sosp variables
-          Sosp android.Product_variables
+	// include Sosp variables
+	Sosp android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
